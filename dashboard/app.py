cc10a8220dfeba33fef96c9fef85015f7db847ca
Janos <jtoldy@web.de>
2025-12-23 07:29:51 +0100

    dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -14,26 +14,53 @@ from api.main import current_focus, daily_stats, weekly_stats, monthly_stats, ov
 API_KEY = os.getenv("API_KEY")
 app = Dash()
 
-# Status Heute
-daily = pd.DataFrame(daily_stats(API_KEY))
-fig = px.bar(daily, x="focus", y="hours")
+default_df = pd.DataFrame(columns=["day","focus","hours","activations"])
 
-app.layout = html.Div(children=[
+# Status Heute
+daily = pd.DataFrame(daily_stats(API_KEY,-1))
+if daily.empty:
+    daily = default_df
+fig_day = px.bar(daily, x="focus", y="hours")
+
+#Status Woche
+weekly = pd.DataFrame(weekly_stats(API_KEY,-1))
+if weekly.empty:
+    weekly = default_df
+fig_week = px.bar(weekly, x="focus", y="hours")
+
+app.layout = html.Div(
+    children=[
+    # Header
     html.Div(
         className="app-header",
         children=[
             html.Div('Time Tracker', className="app-header--title")
         ]
     ),
+    html.Div(),
 
-    html.H2(children='''
-        Today:
-    '''),
+    # Inhalt
+    html.Div(
+        className="app-container",
+        children=[
+        html.Div(children="Today:", className="app-container--title"),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+        dcc.Graph(
+            id='example-graph',
+            figure=fig_day
+        ),
+    ]),
+    html.Div(className="spacer"),
+    html.Div(
+        className="app-container",
+        children=[
+        html.Div(children="Week:", className="app-container--title"),
+
+        dcc.Graph(
+            id='example-graph2',
+            figure=fig_week
+        ),
+    ]),
 ])
 
 if __name__ == '__main__':
